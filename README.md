d98102fb39ad2309c0a7398869682bb19e9b9b44
tybo-dev <tobias@diez.guru git config --global user.name tgit config --global user.email tobias@diez.guru>
2022-11-30 16:40:02 +0100

    styles added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# atm
+# ATM Simulation
+
+## Description
+
+This little application simulates a bank account and atm which allows you to
+deposit and withdraw money.
+
+## Changes
+changes cash back to withdraw, sounded clearer for me
+nice and friendly blue look added
+
--- atm.jsx
+++ atm.jsx
@@ -1,43 +1,105 @@
-const ATMDeposit = ({ onChange, isDeposit }) => {
+const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
   const choice = ["Deposit", "Withdraw"];
-
+  //console.log(`ATM isDeposit: ${isDeposit}`);
   return (
     <label className="label huge">
-      <h3>{choice[Number(!isDeposit)]}</h3>
-      <input type="number" onChange={onChange}></input>
-      <input type="submit" value="Submit"></input>
+      <h3> {choice[Number(!isDeposit)]}</h3>
+      <input
+        id="number-input"
+        type="number"
+        width="200"
+        onChange={onChange}
+      ></input>
+      <input
+        type="submit"
+        disabled={!isValid}
+        width="200"
+        value="Submit"
+        id="submit-input"
+      ></input>
     </label>
   );
 };
 
 const Account = () => {
-  let deposit = 0 ;
+  const [deposit, setDeposit] = React.useState(0);
   const [totalState, setTotalState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
+  const [atmMode, setAtmMode] = React.useState("");
+  const [validTransaction, setValidTransaction] = React.useState(false);
 
-  let status = `Account Balance: $ ${totalState}`;
+  let status = `Account Balance: $ ${totalState} `;
+  //console.log(`Account Rendered with isDeposit: ${isDeposit}`);
 
-  const handleChange = e => {
-    console.log(`handleChange ${e.target.value}`);
-    deposit = Number(e.target.value);
-  };
-  const handleSubmit = () => {
-    let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
-    if (newTotal < 0) {
-      alert("Not enough Cash... GAME OVER!");
-      return;
+  const handleChange = (event) => {
+    let val = Number(event.target.value);
+    setDeposit(val);
+
+    //console.log(`handleChange ${event.target.value}`);
+    if (val <= 0) {
+      return setValidTransaction(false);
     }
-    setTotalState(newTotal);
+
+    if (atmMode === "Withdraw" && val > totalState) {
+      setValidTransaction(false);
+    } else {
+      setValidTransaction(true);
+    }
+
     event.preventDefault();
   };
 
+  const handleSubmit = (event) => {
+    let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
+    setTotalState(newTotal);
+    setValidTransaction(false);
+    
+    event.preventDefault();
+  };
+
+  const handleModeSelect = (event) => {
+    //console.log(event.target.value);
+    setAtmMode(event.target.value);
+    setValidTransaction(false);
+
+    if (event.target.value === "Deposit") {
+      setIsDeposit(true);
+    } else {
+      setIsDeposit(false);
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h2 id="total">{status}</h2>
-      <button onClick={() => setIsDeposit(true)}>Deposit</button>
-      <button onClick={() => setIsDeposit(false)}>Withdraw</button>
-      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
-    </form>
+    <div className="atm">
+      <h1>Best ATM ever!</h1>
+      <form onSubmit={handleSubmit}>
+        
+        <label>Select an option below to continue</label>
+        <select
+          onChange={(e) => handleModeSelect(e)}
+          name="mode"
+          id="mode-select"
+        >
+          <option id="no-selection" value=""></option>
+          <option id="deposit-selection" value="Deposit">
+            Deposit
+          </option>
+          <option id="cashback-selection" value="Withdraw">
+            Withdraw
+          </option>
+        </select>
+
+        <h2 id="total">{status}</h2>
+
+        {atmMode && (
+          <ATMDeposit
+            onChange={handleChange}
+            isDeposit={isDeposit}
+            isValid={validTransaction}
+          ></ATMDeposit>
+        )}
+      </form>
+    </div>
   );
 };
 // ========================================
